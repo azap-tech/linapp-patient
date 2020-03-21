4c9a2f21b6f016aa6b7d4aa751fcd93a6a2a0b94
sconqua <sconqua@gmail.com>
2020-03-21 23:04:58 +0100

    Network service
      #19

--- src/app/network.service.ts
+++ src/app/network.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { MockService } from './mock.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NetworkService {
+  constructor(private mockService: MockService) {}
+  getDepartments(): Observable<string[]> {
+    return this.mockService.getDepartments();
+  }
 
-  constructor() { }
+  getCenters(): Observable<Center[]> {
+    return this.mockService.getCenters();
+  }
+
+  getReasons(): Observable<string[]> {
+    return this.mockService.getReasons();
+  }
+
+  getMinsWaitingTime(): Observable<number> {
+    return this.mockService.getMinsWaitingTime();
+  }
 }
