10e86b783409974302dee5740d2e24ad4b4eff41
sconqua <sconqua@gmail.com>
2020-03-21 22:43:52 +0100

    Store service
      #15

--- src/app/store.service.ts
+++ src/app/store.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
 
+enum Sex {
+  Male,
+  Female
+}
+
+interface UserData {
+  sex: Sex;
+  name: string;
+  reason: string;
+  tel: string;
+  age: number;
+  department: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
+  state: UserData;
 
-  constructor() { }
+  getState() {
+    return this.state;
+  }
+
+  setState() {
+    return this.state;
+  }
+
+  clear() {
+    this.state = null;
+  }
 }
