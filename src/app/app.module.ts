2ca73633a13c1bb2757e4e87c458a345e4ac818a
Giacomo Ferlaino <jack@Giacomos-MacBook-Pro.local>
2020-03-21 21:28:55 +0100

    Added tike page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentComponent } from './pages/department/department.component';
 import { CentersListComponent } from './pages/centers-list/centers-list.component';
+import { TicketComponent } from './pages/ticket/ticket.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DepartmentComponent,
-    CentersListComponent
+    CentersListComponent,
+    TicketComponent
   ],
   imports: [
     BrowserModule,
