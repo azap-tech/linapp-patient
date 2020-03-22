8af3813a14c1c353095ac09f6c5b730741a370b3
antoineccc <antoine.coppin@auspex.ai>
2020-03-22 00:41:18 +0000

    mock confirmation

--- src/app/pages/sms-confirmation/sms-confirmation.component.ts
+++ src/app/pages/sms-confirmation/sms-confirmation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Patient } from './patient';
+
 
 @Component({
   selector: 'app-sms-confirmation',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmsConfirmationComponent implements OnInit {
 
+  PATIENT = {
+    id: '0',
+    name: 'Dupont',
+    title: 'Monsieur',
+    number: '0661664522'
+  };
+
+  patient = this.PATIENT;
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
