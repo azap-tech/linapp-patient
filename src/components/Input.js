6d5fa1c1c1c8805f0d7f3c2785cdc0176f97fa73
Jean Gautier <jgautier@oui.sncf>
2020-03-31 12:54:58 +0200

    feat(input): add regex and check on input *add some css

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,31 +1,31 @@
 import React from "react";
-import { string, number, func } from "prop-types";
+import {func, number, Requireable as bool, string} from "prop-types";
 import styled from "styled-components";
 
 const StyledInput = styled.input`
-    width: 80%;
+  width: 80%;
 `;
 
-const Input = ({label, type, value, onChange}) => {
-  return(
-      <>
-          <label>{label}</label>
-          <StyledInput
-              type={type}
-              value={value}
-              onChange={onChange}
-          />
-      </>
-  )
-};
+const Input = ({ label, type, value, onChange, isInvalid, errorMsg }) => {
 
+  return (
+    <>
+      <label>{label}</label>
+      <StyledInput type={type} value={value} onChange={onChange} />
+    </>
+  );
+};
 
 Input.propType = {
-    label: string.isRequired,
-    type: string.isRequired,
-    value: number.isRequired,
-    onChange: func.isRequired,
+  label: string.isRequired,
+  type: string.isRequired,
+  value: number.isRequired,
+  onChange: func.isRequired,
+  isInvalid: bool,
 };
 
+Input.defaultProps = {
+  isInvalid: false,
+};
 
 export default Input;
--- src/pages/TicketForm.js
+++ src/pages/TicketForm.js
@@ -5,7 +5,7 @@ import { ReactComponent as TicketSVG } from "../assets/ticket.svg";
 import {
   MobileLayout,
   MobileHeader,
-  MobileContent
+  MobileContent,
 } from "../components/MobileLayout";
 import { Button } from "../components/Button";
 import { FormLayout, RadioLayout, Radio } from "../components/FormLayout";
@@ -13,21 +13,62 @@ import Input from "../components/Input";
 import styled from "styled-components";
 
 const DivAge = styled.div`
-    display: flex;
-justify-content: space-between;
-    margin: 20px 0 10px;
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0 0;
 `;
 
 const DivNumber = styled.div`
-    display: flex;
-justify-content: space-between;
-    margin: 10px 0 20px;
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0 0;
 `;
+
+const P = styled.div`
+  margin: 1px;
+  text-align: center;
+  font-family: Avenir;
+  font-style: normal;
+  font-weight: normal;
+  font-size: small;
+  color: #c04f06;
+`;
+
+const VALID_PHONE_REGEX = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/g;
+
+//phoneNumber.length > 10
 
 export function TicketForm({ location, onTicket }) {
   const [sex, setSex] = useState("F");
   const [age, setAge] = useState(null);
   const [phone, setPhone] = useState(null);
+  const [isvalidPhone, setIsvalidPhone] = useState(false);
+  const [isInvalidPhone, setIsInvalidPhone] = useState(false);
+  const [isInvalidAge, setInvalidAge] = useState(false);
+
+  const onPhoneChange = (e) => {
+    const phoneNumber = e.target.value;
+    setPhone(phoneNumber);
+    if (VALID_PHONE_REGEX.test(phoneNumber)) {
+      setIsvalidPhone(true);
+      setIsInvalidPhone(false)
+    } else {
+      setIsvalidPhone(false);
+    }
+    if(phoneNumber.length > 10 && !VALID_PHONE_REGEX.test(phoneNumber)){
+      setIsInvalidPhone(true);
+    }
+  };
+
+  const onAgeChange = (e) => {
+    setAge(e.target.value);
+    const age = parseInt(e.target.value, 10);
+    if (0 < age || age > 200) {
+      setInvalidAge(false);
+    } else {
+      setInvalidAge(true);
+    }
+  };
 
   return (
     <MobileLayout>
@@ -40,25 +81,42 @@ export function TicketForm({ location, onTicket }) {
         <FormLayout>
           <RadioLayout>
             <label>Sex</label>
-            <Radio active={sex === "H"} onClick={e => setSex("H")}>
+            <Radio active={sex === "H"} onClick={(e) => setSex("H")}>
               Homme
             </Radio>
-            <Radio active={sex === "F"} onClick={e => setSex("F")}>
+            <Radio active={sex === "F"} onClick={(e) => setSex("F")}>
               Femme
             </Radio>
           </RadioLayout>
-          <DivAge>
-            <Input label="Âge" type="number" value={age} onChange={evt => setAge(evt.target.value)} />
-          </DivAge>
+          <div>
+            <DivAge>
+              <Input
+                label="Âge"
+                type="number"
+                value={age}
+                onChange={onAgeChange}
+              />
+            </DivAge>
+            {isInvalidAge && <P>Veuillez renseigner un âge valide</P>}
+          </div>
+          <div>
           <DivNumber>
-            <Input label="Mobile" type="phone" value={phone} onChange={evt => setPhone(evt.target.value)} />
+            <Input
+              label="Mobile"
+              type="phone"
+              value={phone}
+              onChange={onPhoneChange}
+              isInvalid={isInvalidPhone}
+            />
           </DivNumber>
+            {isInvalidPhone && <P>Veuillez renseigner un numéro de téléphone valide</P>}
+          </div>
         </FormLayout>
         <p>
           En cliquant sur le bouton ci-dessous j’accepte les conditions
           générales d’utilisation du service
         </p>
-        <Button onClick={e => onTicket({ sex, age: parseInt(age), phone })}>
+        <Button isDisabeled={isInvalidAge || !isvalidPhone} onClick={(e) => (!isInvalidAge && isvalidPhone) && onTicket({ sex, age: parseInt(age), phone })}>
           Valider
         </Button>
       </MobileContent>
